ac15a3cecfb25885ca6636fcedf3055564603803
Annania Plossl <nia.plossl@gmail.com>
2024-04-23 12:51:29 -0400

    completed the lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,match_list):
+        return_list = []
+        for item in match_list:
+            if(sorted([*self.word])==sorted([*item])):
+                return_list.append(item)
+        return return_list
